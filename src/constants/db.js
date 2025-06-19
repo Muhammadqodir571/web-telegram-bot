66e9232158a200e6fa627d1f37518b22354d0337
My name is Usmonaliyev Muhammadqodir <138517078+Muhammadqodir571@users.noreply.github.com>
2025-06-19 19:07:36 +0500

    db qoshildi

--- src/constants/db.js
+++ src/constants/db.js
@@ -11,35 +11,35 @@ export function getData() {
 			title: 'ReactJS',
 			price: 15,
 			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2F65rcPxsLT9ysJDisXF80&w=3840&q=75',
+				'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8wh_vrysQFY_0CkcD-e-lYPPHLvsTaXqIGA&s',
 			id: 2,
 		},
 		{
 			title: 'VueJS',
 			price: 13.5,
 			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2F54vR0DStGoFuZBVkFwWQ&w=3840&q=75',
+				'https://images.prismic.io/northcoders/NzIxNjIyM2ItNDY0NC00MWRhLWEzM2UtM2IyZThkM2JhMWNl_vuejs.jpeg?auto=compress%2Cformat&rect=0%2C0%2C1280%2C720&w=1680&h=945&fit=max&q=605',
 			id: 3,
 		},
 		{
 			title: 'NodeJS',
 			price: 13.99,
 			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FmDcwhbguQpyM74jb18M5&w=3840&q=75',
+				'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRv3PGEyeJbr65Z2XC-eBGIJvQdtpg0L7XkA&s',
 			id: 4,
 		},
 		{
 			title: 'NestJS',
 			price: 12.5,
 			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FxN7aKkLMQUuD72ikvC1H&w=3840&q=75',
+				'https://miro.medium.com/v2/resize:fit:1200/1*uOMFBvRtzcFntZgO35RU2Q.png',
 			id: 5,
 		},
 		{
 			title: 'NextJS',
 			price: 12.99,
 			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FPe94ubZPSCHgXDIPBxTr&w=3840&q=75',
+				'https://codewithmosh.com/_next/image?url=https%3A%2F%2Fcdn.filestackcontent.com%2F8MbtJ4hTAaOk3KPcptqZ&w=3840&q=75',
 			id: 6,
 		},
 		{
@@ -53,8 +53,93 @@ export function getData() {
 			title: 'TensorflowJS',
 			price: 28.99,
 			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FtIMgibghQveiDeJeXDPI&w=3840&q=75',
+				'https://miro.medium.com/v2/resize:fit:1200/0*IduxcgcHeIt_KOyg.png',
 			id: 8,
 		},
+		{
+			title: 'Exsperss.JS',
+			price: 30.00,
+			Image:
+				'https://cdn.hashnode.com/res/hashnode/image/upload/v1675637255386/f3a9a38b-116d-4b35-8f46-8d8abb78166f.png',
+			id: 9,
+		},
+		{
+			title: 'Java',
+			price: 35.35,
+			Image:
+				'https://cdn-clekk.nitrocdn.com/tkvYXMZryjYrSVhxKeFTeXElceKUYHeV/assets/images/optimized/rev-cc9e27e/litslink.com/wp-content/uploads/2020/11/what-is-java-image.png',
+			id: 10,
+		},
+		{
+			title: 'Python',
+			price: 40.01,
+			Image:
+				'https://beecrowd.com/wp-content/uploads/2024/04/2022-07-19-Melhores-cursos-de-Python.jpg',
+			id: 11,
+		},
+		{
+			title: 'C++',
+			price: 25.02,
+			Image:
+				'https://reviewsreporter.com/wp-content/uploads/2022/08/CPlusPlus1.jpg',
+			id: 12,
+		},
+		{
+			title: 'PHP',
+			price: 28.99,
+			Image:
+				'https://bs-uploads.toptal.io/blackfish-uploads/components/blog_post_page/4085715/cover_image/retina_1708x683/0925-3D_Data_Visualization_with_Open_Source_Tools_A_Tutorial_Using_VTK_Dan_Newsletter-0a7d5944210766ea5745cce80218ad3a.png',
+			id: 13,
+		},
+		{
+			title: 'Django',
+			price: 13.99,
+			Image:
+				'https://www.coderio.com/wp-content/uploads/2024/12/Django-The-Python-Web-Framework.jpg',
+			id: 14,
+		},
+		{
+			title: 'Telegram bot ',
+			price: 34.99,
+			Image:
+				'https://i0.wp.com/www.alphr.com/wp-content/uploads/2021/05/How-to-Add-a-Bot-in-Telegram.jpg?fit=602%2C372&ssl=1',
+			id: 15,
+		},
+		{
+			title: 'Cyber Security',
+			price: 34.54 ,
+			Image:
+				'https://www.neit.edu/wp-content/uploads/2022/10/Cyber-Security-Icon-Concept-2-1.jpeg',
+			id: 16,
+		},
+		{
+			title: 'Data analitika',
+			price: 30.99,
+			Image:
+				'https://najottalim.uz/_next/image?url=https%3A%2F%2Fnws.cdn.najottalim.uz%2Fmain-server%2Fposts%2F5f16dbd2-62a9-4542-8f47-84f2635fdbbf.png&w=3840&q=100',
+			id: 17,
+		},
+		{
+			title: 'Wordperss',
+			price: 28.99,
+			Image:
+				'https://eson.uz/wp-content/uploads/2021/06/wp.png',
+			id: 18,
+		},
+		{
+			title: 'Postgressql',
+			price: 28.99,
+			Image:
+				'https://serverflow.ru/upload/iblock/3d5/egzmidpizbtch1m1klyz0xkqkucrvjs5/postgresql-cover.png',
+			id: 19,
+		},
+		{
+			title: 'NO SQL',
+			price: 28.99,
+			Image:
+				'https://www.opc-router.de/wp-content/uploads/2021/04/mongoDB_socialmedia_en.jpg',
+			id: 20,
+		},
+		
 	];
 }
